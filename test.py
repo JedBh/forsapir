c23c9b912b46c947fb7e716149c186ca742ccfbc
JedBh <44101778+JedBh@users.noreply.github.com>
2021-01-15 15:27:40 +0200

    Aaaaaaaaa work

--- test.py
+++ test.py
@@ -3,3 +3,7 @@ age = input("Please enter your age: ")
 
 print("\nHello " + str(name) + ", you are " + str(age) + " years old!")
 print("Doing that to see what's up with Visual Studio Code.")
+
+
+for i in range(0, 10):
+    print("This does not work for the " + str(i) + " time.")
