2d1888232c07a4f446d5e3f2f04a4758d769f980
JedBh <44101778+JedBh@users.noreply.github.com>
2021-01-15 15:19:26 +0200

    Hey does this work?

--- test.py
+++ test.py
@@ -2,3 +2,4 @@ name = input("Please enter your name: ")
 age = input("Please enter your age: ")
 
 print("\nHello " + str(name) + ", you are " + str(age) + " years old!")
+print("Doing that to see what's up with Visual Studio Code.")
